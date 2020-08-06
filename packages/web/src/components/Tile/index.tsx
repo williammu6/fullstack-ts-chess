5bfe9b6f045162f1cece3594f15c8423dc0d6c9d
William Murari <williammu6@gmail.com>
2020-08-05 21:44:57 -0300

    board sort of responsive

--- packages/web/src/components/Tile/index.tsx
+++ packages/web/src/components/Tile/index.tsx
@@ -4,8 +4,7 @@ const Tile = styled.div.attrs({
   className: "border border-solid border-2 border-gray-300"
 })`
   background: white;
-  width: 4.5rem;
-  height: 4.5rem;
+  flex: 1;
 `;
 
 export default Tile;
--- packages/web/src/components/board/index.tsx
+++ packages/web/src/components/board/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import Tile from "../Tile";
 
+import "./styles.css";
+
 const Board = () => {
   return (
-    <div className="border-solid border-4 border-gray-600">
+    <div className="border-solid border-4 border-gray-600 board">
       {Array.from({ length: 8 }).map(() => (
-        <div className="flex flex-row">
+        <div className="flex flex-row row">
           {Array.from({ length: 8 }).map(() => (
             <Tile />
           ))}
