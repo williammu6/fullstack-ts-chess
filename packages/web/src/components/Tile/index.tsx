84fe5dfaea1a98dae5c091128023ca46922dbfb7
William Murari <williammu6@gmail.com>
2020-08-05 22:13:47 -0300

    colored tiles

--- packages/web/src/components/Tile/index.tsx
+++ packages/web/src/components/Tile/index.tsx
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 
 type Props = {
-  dark?: boolean;
+  col: number;
+  row: number;
+};
+
+const isDark = (row: number, col: number) => {
+  return (row % 2 === 0 && col % 2 === 1) || (row % 2 === 1 && col % 2 === 0);
 };
 
 const Tile = styled.div.attrs({
   className: ""
 })`
-  background: ${(p: Props) => (p.dark ? "gray" : "white")};
+  background: ${(p: Props) => (isDark(p.row, p.col) ? "gray" : "white")};
   flex: 1;
 `;
+
 export default Tile;
--- packages/web/src/components/board/index.tsx
+++ packages/web/src/components/board/index.tsx
@@ -5,16 +5,12 @@ import Tile from "../Tile";
 import "./styles.css";
 
 const Board = () => {
-  const isDark = (row: number, col: number) => {
-    return (row % 2 === 0 && col % 2 === 0) || (row % 2 === 1 && col % 2 === 1);
-  };
-
   return (
     <div className="border-solid border-4 border-gray-700 board">
       {Array.from({ length: 8 }).map((_, row) => (
         <div className="flex flex-row row">
           {Array.from({ length: 8 }).map((_, col) => (
-            <Tile dark={isDark(row, col)} />
+            <Tile row={row} col={col} />
           ))}
         </div>
       ))}
